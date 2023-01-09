4118bf0cc982605bc52278f9cb8a51007822a626
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:42:14 +0300

    Add files via upload

---
+++ if2.java
@@ -0,0 +1,14 @@
+public class if2 {
+    public static void main(String[] args) {
+        int r=15;
+        if (r<5) {
+            System.out.println("Верно");
+        } else if (r>15) {
+            System.out.println("Не верно");
+        } else{
+                System.out.println("Ничего");
+            }
+        }
+    }
+
+
