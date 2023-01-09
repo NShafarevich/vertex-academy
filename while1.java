2fbd71f205fce798716a0b248a7d975fca25b193
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:42:45 +0300

    Add files via upload

---
+++ while1.java
@@ -0,0 +1,10 @@
+public class while1 {
+    public static void main(String[] args) {
+        int r = 7;
+        while (r <= 98) {
+            System.out.print(r +" ");
+            r = r+7;
+        }
+    }
+}
+
