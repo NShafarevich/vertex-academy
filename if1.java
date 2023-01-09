a14c8b480d99f85b3d7235bd101061224305dbb7
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:42:02 +0300

    Add files via upload

---
+++ if1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class if1 {
+    public static void main(String[] args) {
+        int r = 15;
+        if (r < 5) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Не верно");
+        }
+    }
+}
