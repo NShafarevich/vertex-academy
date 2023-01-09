4731a02d08c930fb4563c496a9770753b9340313
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:42:56 +0300

    Add files via upload

---
+++ while2.java
@@ -0,0 +1,10 @@
+public class while2 {
+    public static void main(String[] args) {
+        int r = 1;
+        while (r <= 512) {
+            System.out.print(r + " ");
+            r = r*2;
+        }
+    }
+}
+
