0ebb99a2d8fe0022e4a0d137c1ff4d1f2f1f56f2
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:41:48 +0300

    Add files via upload

---
+++ for5.java
@@ -0,0 +1,10 @@
+public class for5 {
+    public static void main(String[] args) {
+        for (int t = 0; t < 3; t++){
+            for (int r = 0; r < 5; r++) {
+                System.out.print("* ");
+            }
+       System.out.println();
+        }
+    }
+}
