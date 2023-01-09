3fed66057371266e16f86eaead1b8affe268e3fc
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:41:24 +0300

    Add files via upload

---
+++ for3.java
@@ -0,0 +1,8 @@
+public class for3 {
+    public static void main(String[] args) {
+        for (int r=1; r<= 10; r++) {
+            System.out.println ("3*" + r + "=" + 3*r);
+        }
+    }
+}
+
